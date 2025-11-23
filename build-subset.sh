6b46b96352c6e57fbb8137e9b856727c729ae8e4
LANMIN-X <155052516+LANMIN-X@users.noreply.github.com>
2025-11-23 18:20:56 +0800

    Update build-subset.sh

--- build-subset.sh
+++ build-subset.sh
@@ -2,19 +2,21 @@
 set -e
 
 em++ \
-	-std=c++11 \
-	-fno-exceptions \
-	-fno-rtti \
-	-fno-threadsafe-statics \
-	-fvisibility-inlines-hidden \
-	-Oz \
-	-I. \
-	-DHB_TINY \
-	-DHB_USE_INTERNAL_QSORT \
-	-DHB_CONFIG_OVERRIDE_H=\"config-override-subset.h\" \
-	-DHB_EXPERIMENTAL_API \
-	--no-entry \
-	-s EXPORTED_FUNCTIONS=@hb-subset.symbols \
-	-s INITIAL_MEMORY=65MB \
-	-o hb-subset.wasm \
-	harfbuzz/src/harfbuzz-subset.cc
+    -std=c++11 \
+    -fno-exceptions \
+    -fno-rtti \
+    -fno-threadsafe-statics \
+    -fvisibility-inlines-hidden \
+    -O2 \
+    -I. \
+    -DHB_TINY \
+    -DHB_USE_INTERNAL_QSORT \
+    -DHB_CONFIG_OVERRIDE_H=\"config-override-subset.h\" \
+    -DHB_EXPERIMENTAL_API \
+    --no-entry \
+    -s EXPORTED_FUNCTIONS=@hb-subset.symbols \
+    -s ALLOW_MEMORY_GROWTH=1 \
+    -s INITIAL_MEMORY=256MB \
+    -s MAXIMUM_MEMORY=1GB \
+    -o hb-subset.wasm \
+    harfbuzz/src/harfbuzz-subset.cc
