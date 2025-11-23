2d88ceeed279628b82c6f33981a2666780769e65
LANMIN-X <155052516+LANMIN-X@users.noreply.github.com>
2025-11-23 18:41:50 +0800

    Update hb-subset.example.node.js

--- examples/hb-subset.example.node.js
+++ examples/hb-subset.example.node.js
@@ -1,66 +1,68 @@
-// Based on https://github.com/harfbuzz/harfbuzzjs/issues/9#issuecomment-507874962
-// Which was based on https://github.com/harfbuzz/harfbuzzjs/issues/9#issuecomment-507622485
-const { readFile, writeFile } = require('fs').promises;
-const { join, extname, basename } = require('path');
-const { performance } = require('node:perf_hooks');
+const fs = require("fs");
+const path = require("path");
 
-const SUBSET_TEXT = 'abc';
+// 关键：必须加载 hb-subset.js，而不是 wasm
+const createHBSubset = require("../hb-subset.js");
 
-(async () => {
-    const { instance: { exports } } = await WebAssembly.instantiate(await readFile(join(__dirname, '../hb-subset.wasm')));
-    const fileName = 'NotoSans-Regular.ttf';
-    const fontBlob = await readFile(join(__dirname, '../test/fonts/noto', fileName));
+const SUBSET_TEXT = "abc";
 
-    const t = performance.now();
-    const heapu8 = new Uint8Array(exports.memory.buffer);
-    const fontBuffer = exports.malloc(fontBlob.byteLength);
-    heapu8.set(new Uint8Array(fontBlob), fontBuffer);
+async function main() {
+  const hb = await createHBSubset(); // ← Emscripten 工厂函数，返回包含 wasm 实例的对象
 
-    /* Creating a face */
-    const blob = exports.hb_blob_create(fontBuffer, fontBlob.byteLength, 2/*HB_MEMORY_MODE_WRITABLE*/, 0, 0);
-    const face = exports.hb_face_create(blob, 0);
-    exports.hb_blob_destroy(blob);
+  console。log("hb-subset wasm loaded");
 
-    /* Add your glyph indices here and subset */
-    const input = exports.hb_subset_input_create_or_fail();
-    const unicode_set = exports.hb_subset_input_unicode_set(input);
-    for (const text of SUBSET_TEXT) {
-        exports.hb_set_add(unicode_set, text.codePointAt(0));
-    }
+  // 读取字体数据
+  const fileName = "NotoSans-Regular.ttf";
+  const fontPath = path.join(__dirname， "../test/fonts/noto"， fileName);
+  const fontBlob = fs.readFileSync(fontPath);
 
-    // exports.hb_subset_input_set_drop_hints(input, true);
-    const subset = exports.hb_subset_or_fail(face, input);
+  // 分配内存
+  const ptr = hb._malloc(fontBlob。length);
+  hb。HEAPU8。set(fontBlob， ptr);
 
-    /* Clean up */
-    exports.hb_subset_input_destroy(input);
+  // 创建 blob / face
+  const blob = hb._hb_blob_create(ptr, fontBlob.length, 2, 0, 0);
+  const face = hb._hb_face_create(blob， 0);
+  hb。_hb_blob_destroy(blob);
 
-    /* Get result blob */
-    const resultBlob = exports.hb_face_reference_blob(subset);
+  // subset input
+  const input = hb._hb_subset_input_create_or_fail();
+  const unicodeSet = hb._hb_subset_input_unicode_set(input);
 
-    const offset = exports.hb_blob_get_data(resultBlob, 0);
-    const subsetByteLength = exports.hb_blob_get_length(resultBlob);
-    if (subsetByteLength === 0) {
-        exports.hb_blob_destroy(resultBlob);
-        exports.hb_face_destroy(subset);
-        exports.hb_face_destroy(face);
-        exports.free(fontBuffer);
-        throw new Error(
-            'Failed to create subset font, maybe the input file is corrupted?'
-        );
-    }
-    
-    // Output font data(Uint8Array)
-    const subsetFontBlob = heapu8.subarray(offset, offset + exports.hb_blob_get_length(resultBlob));
-    console.info('✨ Subset done in', performance.now() - t, 'ms');
+  for (const ch / SUBSET_TEXT) {
+    hb._hb_set_add(unicodeSet, ch.codePointAt(0));
+  }
 
-    const extName = extname(fileName).toLowerCase();
-    const fontName = basename(fileName, extName);
-    await writeFile(join(__dirname, '/', `${fontName}.subset${extName}`), subsetFontBlob);    
-    console.info(`Wrote subset to: ${__dirname}/${fontName}.subset${extName}`);
+  const subsetFace = hb._hb_subset_or_fail(face, input);
+  hb._hb_subset_input_destroy(input);
 
-    /* Clean up */
-    exports.hb_blob_destroy(resultBlob);
-    exports.hb_face_destroy(subset);
-    exports.hb_face_destroy(face);
-    exports.free(fontBuffer);
-})();
+  // 获取结果 blob
+  const resultBlob = hb._hb_face_reference_blob(subsetFace);
+  const resultPtr = hb._hb_blob_get_data(resultBlob, 0);
+  const resultLen = hb。_hb_blob_get_length(resultBlob);
+
+  if (!resultLen) {
+    throw new 错误("Subset failed – check font file");
+  }
+
+  // 读取返回的子集字体二进制
+  const subsetData = hb.HEAPU8.slice(resultPtr, resultPtr + resultLen);
+
+  // 写文件
+  const outName = path.basename(fileName, path.extname(fileName)) + ".subset.ttf";
+  const outPath = path.join(__dirname, outName);
+
+  fs.writeFileSync(outPath, Buffer.from(subsetData));
+  console。log("Wrote subset:"， outPath);
+
+  // clean up
+  hb._hb_blob_destroy(resultBlob);
+  hb._hb_face_destroy(subsetFace);
+  hb._hb_face_destroy(face);
+  hb._free(ptr);
+}
+
+main().catch(err => {
+  console.error(err);
+  process.exit(1);
+});
