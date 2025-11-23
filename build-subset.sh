85dcb7187cfd4346a7bdce401a0756c85677be7c
LANMIN-X <155052516+LANMIN-X@users.noreply.github.com>
2025-11-23 18:57:16 +0800

    Update build-subset.sh

--- build-subset.sh
+++ build-subset.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 set -e
 
+# 1. 确保使用 dlmalloc (默认)，不要用 emmalloc (虽然小但容易碎片化)
+# 2. 移除 -DHB_TINY，如果是做工具，最好保留完整功能以兼容各种怪异字体
+# 3. 增加栈空间，防止深层递归爆栈
+
 em++ \
     -std=c++11 \
+    -O3 \
+    -flto \
     -fno-exceptions \
     -fno-rtti \
     -fno-threadsafe-statics \
     -fvisibility-inlines-hidden \
-    -O2 \
     -I. \
-    -DHB_TINY \
     -DHB_USE_INTERNAL_QSORT \
     -DHB_CONFIG_OVERRIDE_H=\"config-override-subset.h\" \
     -DHB_EXPERIMENTAL_API \
     --no-entry \
     -s EXPORTED_FUNCTIONS=@hb-subset.symbols \
     -s ALLOW_MEMORY_GROWTH=1 \
-    -s INITIAL_MEMORY=256MB \
-    -s MAXIMUM_MEMORY=1GB \
-    -s MODULARIZE=1 \
-    -s EXPORT_ES6=0 \
-    -o hb-subset.js \
+    -s INITIAL_MEMORY=64MB \
+    -s MAXIMUM_MEMORY=2GB \
+    -s STACK_SIZE=5MB \
+    -s ABORTING_MALLOC=0 \
+    -s MALLOC="dlmalloc" \
+    -o hb-subset.wasm \
     harfbuzz/src/harfbuzz-subset.cc
